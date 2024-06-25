a9226c5dc19b9925871ae85eca7228117e7f7f98
Patrick Mifsud <patrickmifsud@outlook.com>
2024-06-25 12:45:09 +1000

    Removed FeaturedNavigation

--- SnipeClientMac/Navigation/AppSidebarList.swift
+++ SnipeClientMac/Navigation/AppSidebarList.swift
@@ -35,40 +35,6 @@ struct AppSidebarList: View {
     }
 }
 
-struct FeaturedNavigation: View {
-    var symbol: String
-    var type: SymbolRenderingMode?
-    var color: Color
-    var label: String
-    var count: Int = 0
-    
-    var body: some View {
-        HStack(alignment: .top) {
-            VStack(alignment: .leading, spacing: 15) {
-                Image(systemName: symbol)
-                    .font(.title)
-                    .symbolVariant(.fill)
-                    .foregroundColor(color)
-                    .symbolRenderingMode(type ?? .monochrome)
-                Text(label)
-                    .font(.body)
-                    .foregroundColor(.primary)
-                    .fontWeight(.medium)
-                    .foregroundColor(.primary)
-            }
-            Spacer()
-            if count != 0 {
-                Text("\(count)")
-                    .font(.system(.title, design: .rounded))
-                    .fontWeight(.semibold)
-                    .foregroundColor(.secondary)
-            }
-        }
-        .frame(height: 70)
-    }
-}
-
-
 #Preview {
     NavigationSplitView {
         AppSidebarList(selection: .constant(.dashboard))
