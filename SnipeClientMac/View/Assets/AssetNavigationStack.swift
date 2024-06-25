18a37d9de1ed4b072ca420b75232adfc9327e27c
Patrick Mifsud <patrickmifsud@outlook.com>
2024-06-25 12:44:44 +1000

    Created new Asset View for iPhone

--- SnipeClientMac/View/Assets/AssetNavigationStack.swift
+++ SnipeClientMac/View/Assets/AssetNavigationStack.swift
@@ -12,7 +12,7 @@ struct AssetNavigationStack: View {
         NavigationStack {
             Group {
                 #if os(iOS)
-                AssetListView()
+                AssetsView()
                 #else
                 AssetTableView()
                 #endif
@@ -26,10 +26,13 @@ struct AssetCategoryListView: View {
     @StateObject private var viewModel = SnipeAPIService()
     
     var body: some View {
-        Group {
+        VStack(alignment: .leading, spacing: 5) {
+            Text("Categories")
+                .font(.headline)
             ForEach(viewModel.categoryItem) { category in
-                VStack {
+                HStack {
                     Text(category.name)
+                    Spacer()
                     Text("\(category.assetsCount)")
                 }
             }
@@ -37,6 +40,68 @@ struct AssetCategoryListView: View {
         .onAppear {
             viewModel.fetchCategories(category: "Asset")
             print(viewModel.hardwareItems)
+        }
+    }
+}
+
+struct AssetsView: View {
+    @StateObject private var service = SnipeAPIService()
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            HStack {
+                FeaturedNavigation(symbol: "laptopcomputer.and.iphone", color: .blue, label: "Hardware", count: "\(service.hardwareTotal)")
+                FeaturedNavigation(symbol: "cube.box", color: .blue, label: "Accessories")
+            }
+            HStack {
+                FeaturedNavigation(symbol: "cpu", color: .green, label: "Components")
+                FeaturedNavigation(symbol: "drop.halffull", color: .orange, label: "Consumables")
+            }
+            AssetCategoryListView()
+            Spacer()
+        }
+        .padding()
+        .navigationTitle("Assets")
+        .onAppear {
+            service.fetchHardware()
+        }
+        .refreshable {
+            service.fetchHardware()
+        }
+    }
+}
+
+struct FeaturedNavigation: View {
+    var symbol: String
+    var type: SymbolRenderingMode?
+    var color: Color
+    var label: String
+    var count: String?
+    
+    var body: some View {
+        GroupBox {
+            HStack(alignment: .top) {
+                VStack(alignment: .leading, spacing: 15) {
+                    Image(systemName: symbol)
+                        .font(.title)
+                        .symbolVariant(.fill)
+                        .foregroundColor(color)
+                        .symbolRenderingMode(type ?? .monochrome)
+                    Text(label)
+                        .font(.body)
+                        .foregroundColor(.primary)
+                        .fontWeight(.medium)
+                        .foregroundColor(.primary)
+                }
+                Spacer()
+                if count != "" {
+                    Text(count ?? "")
+                        .font(.system(.title, design: .rounded))
+                        .fontWeight(.semibold)
+                        .foregroundColor(.secondary)
+                }
+            }
+            .frame(height: 70)
         }
     }
 }
