b104f18759b4a966c13b1ded051c5ab32966497a
Patrick Mifsud <patrickmifsud@outlook.com>
2024-07-18 15:49:52 +1000

    Shows user in a list. Paginates results. Added search

--- SnipeClientMac/View/Users/UserListView.swift
+++ SnipeClientMac/View/Users/UserListView.swift
@@ -8,34 +8,68 @@
 import SwiftUI
 
 struct UserListView: View {
-    @StateObject private var viewModel = SnipeAPIService()
+    @StateObject private var service = SnipeAPIService()
     
+    @State private var searchTerm: String = ""
+
     var body: some View {
-        List(viewModel.userItem) { user in
-            UserRowView(
-                fName: user.firstName,
-                lName: user.lastName,
-                employeeNum: user.employeeNum
-            )
+        List(service.userItem) { users in
+            HStack {
+                AsyncImage(url: URL(string: users.avatar ?? "")) { image in
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .padding(5)
+                } placeholder: {
+                    Image(systemName: "person.crop.circle")
+                        .font(.system(size: 50))
+                }
+                .frame(width: 60, height: 60)
+                .padding(5)
+                .background(.white, in: RoundedRectangle(cornerRadius: 15, style: .continuous))
+                VStack(alignment: .leading) {
+                    Text("\(users.name)")
+                        .font(.headline)
+                    if let usersCode = users.employeeNum {
+                        Text("\(usersCode)")
+                            .font(.subheadline)
+                            .fontWeight(.medium)
+                            .foregroundColor(.secondary)
+                    }
+                    Text("Assets: \(users.assetsCount)")
+                }
+                Spacer()
+            }
+            .onAppear {
+                if users == service.userItem.last {
+                    service.fetchUsers(offset: service.userItem.count)
+                }
+            }
         }
         .onAppear {
-            viewModel.fetchUsers()
+            service.fetchUsers()
         }
         .refreshable {
-            viewModel.fetchUsers()
+            service.fetchUsers()
         }
+        .onSubmit(of: .search) {
+            service.fetchUsers(searchTerm: searchTerm)
+        }
+        .onChange(of: searchTerm) { oldTerm, newTerm in
+            service.fetchUsers(searchTerm: searchTerm)
+        }
+        .searchable(text: $searchTerm, prompt: "Search")
     }
 }
     
 struct UserRowView: View {
-    var fName: String
-    var lName: String
+    var name: String
     var employeeNum: String
     
     var body: some View {
         HStack(spacing: 10) {
             VStack {
-                Text("\(fName) \(lName)")
+                Text("\(name)")
                 Text("\(employeeNum)")
                     .foregroundStyle(.secondary)
             }
--- SnipeClientMac/View/Users/UserTableView.swift
+++ SnipeClientMac/View/Users/UserTableView.swift
@@ -8,80 +8,101 @@
 import SwiftUI
 
 struct UserTableView: View {
-    @StateObject private var viewModel = SnipeAPIService()
+    @StateObject private var service = SnipeAPIService()
     
     @SceneStorage("AssetTableConfig")
     private var columnCustomization: TableColumnCustomization<User>
     
     @State private var selection: User.ID?
+    @State private var searchTerm: String = ""
     
     var body: some View {
         VStack {
-            if let error = viewModel.errorMessage {
-                Text("Error: \(error.message)")
-                    .foregroundColor(.red)
-            } else {
-                Table(viewModel.userItem, selection: $selection, columnCustomization: $columnCustomization) {
-                    TableColumn("First Name") { user in
-                        Text(user.firstName)
+            Table(service.userItem, selection: $selection, columnCustomization: $columnCustomization) {
+                TableColumn("First Name") { user in
+                    Text(user.firstName)
+                }
+                .customizationID("firstName")
+                TableColumn("Last Name") { user in
+                    Text(user.lastName ?? "")
+                }
+                .customizationID("lastName")
+                TableColumn("Date Created") { user in
+                    Text(user.createdAt?.datetime ?? "")
+                }
+                .customizationID("employeeNum")
+                TableColumn("ID Number") { user in
+                    Text(user.employeeNum  ?? "")
+                }
+                .customizationID("employeeNum")
+                TableColumn("Username") { user in
+                    Text(user.username)
+                }
+                .customizationID("username")
+                TableColumn("Email") { user in
+                    Text("\(user.email ?? "No Email")")
+                }
+                .customizationID("email")
+                TableColumn("Assets") { user in
+                    Text("\(user.assetsCount)")
+                }
+                .customizationID("assetsCount")
+                TableColumn("Licenses") { user in
+                    Text("\(user.licensesCount)")
+                }
+                .customizationID("licensesCount")
+                TableColumn("Consumables") { user in
+                    Text("\(user.consumablesCount)")
+                }
+                .customizationID("consumablesCount")
+            }
+            .onAppear {
+                if service.userItem.isEmpty {
+                    service.fetchUsers()
+                }
+            }
+            .onSubmit(of: .search) {
+                service.fetchUsers(searchTerm: searchTerm)
+            }
+            .onChange(of: searchTerm) { oldTerm, newTerm in
+                service.fetchUsers(searchTerm: searchTerm)
+            }
+            .onChange(of: service.userItem) { oldItems, newItems in
+                if newItems.last != nil {
+                    DispatchQueue.main.async {
+                        if newItems.count < service.userTotal {
+                            service.fetchUsers(offset: newItems.count)
+                        }
                     }
-                    .customizationID("firstName")
-                    TableColumn("Last Name") { user in
-                        Text(user.lastName)
-                    }
-                    .customizationID("lastName")
-                    TableColumn("Employee Number") { user in
-                        Text(user.employeeNum)
-                    }
-                    .customizationID("employeeNum")
-                    TableColumn("Username") { user in
-                        Text(user.username)
-                    }
-                    .customizationID("username")
-                    TableColumn("Email") { user in
-                        Text("\(user.email)")
-                    }
-                    .customizationID("email")
-                    TableColumn("Assets") { user in
-                        Text("\(user.assetsCount)")
-                    }
-                    .customizationID("assetsCount")
-                    TableColumn("Licenses") { user in
-                        Text("\(user.licensesCount)")
-                    }
-                    .customizationID("licensesCount")
-                    TableColumn("Consumables") { user in
-                        Text("\(user.consumablesCount)")
-                    }
-                    .customizationID("consumablesCount")
                 }
             }
         }
-        .onAppear {
-            viewModel.fetchUsers()
-        }
-        .refreshable {
-            viewModel.fetchUsers()
-        }
-        .alert(item: $viewModel.errorMessage) { error in
+        .alert(item: $service.errorMessage) { error in
             Alert(
                 title: Text("Unable to retrieve Users"),
                 message: Text(error.message),
                 primaryButton: .default(Text("Retry"), action: {
-                    viewModel.fetchUsers()
+                    service.fetchUsers()
                 }),
                 secondaryButton: .cancel()
             )
         }
         .toolbar {
             ToolbarItem(placement: .automatic) {
-                Button(action: {
-                    viewModel.fetchUsers()
-                }, label: {
-                    Label("Refresh", systemImage: "arrow.clockwise")
-                })
+                if service.isLoading {
+                    ProgressView()
+                        .scaleEffect(0.6)
+                        .progressViewStyle(.circular)
+                } else {
+                    Button(action: {
+                        service.fetchUsers()
+                    }, label: {
+                        Label("Refresh", systemImage: "arrow.clockwise")
+                    })
+                }
             }
         }
+        .searchable(text: $searchTerm, prompt: "Search")
     }
 }
 
