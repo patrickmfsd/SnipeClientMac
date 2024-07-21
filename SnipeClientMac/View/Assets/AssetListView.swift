63ac1a331ca0f9e0fed72e493a3651f070abbb2f
Patrick Mifsud <patrickmifsud@outlook.com>
2024-07-21 21:05:58 +1000

    Split Asset Row View to its ownfiles

--- SnipeClientMac/View/Assets/AssetListView.swift
+++ SnipeClientMac/View/Assets/AssetListView.swift
@@ -49,43 +49,6 @@ struct AssetListView: View {
     }
 }
 
-struct AssetRowView: View {
-    var image: String
-    var name: String
-    var manufacturer: String
-    var model: String
-    var assetNumber: String
-    var serialNumber: String
-
-    var body: some View {
-        HStack(spacing: 10) {
-            AsyncImage(url: URL(string: image)) { image in
-                image
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 80, height: 80)
-                    .padding(5)
-                    .background(.white, in: RoundedRectangle(cornerRadius: 15, style: .continuous))
-            } placeholder: {
-                Image(systemName: "laptopcomputer")
-                    .font(.system(size: 50))
-            }
-            VStack(alignment: .leading) {
-                if name.isEmpty {
-                    Text(assetNumber.isEmpty ? assetNumber : serialNumber)
-                        .font(.headline)
-                } else {
-                    Text(name)
-                        .font(.headline)
-                }
-                Text("\(manufacturer) - \(model)")
-                    .foregroundStyle(.secondary)
-            }
-            Spacer()
-        }
-    }
-}
-
 #Preview {
     AssetListView()
 }
